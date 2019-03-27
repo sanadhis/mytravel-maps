e09a482b67423c3ae6c3e2e0bb52cc65212af861
sanadhis <sanadhis@gmail.com>
2019-03-27 10:15:19 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-// var map = new Datamap({
-//   element: document.getElementById('container'),
-//   fills: {
-//     defaultFill: '#74b9ff'
-//   }
-// });
-// $.get('data/travel.csv', function(data){
-//     var test = $.csv.toObjects(data);
-//     console.log(test)
-// })
+$(document).ready(function(){
 
-var map = new Datamap({
-    element: document.getElementById('container'),
-    responsive: true,
-    fills: {
-        VISITED: '#fab1a0',
-        defaultFill: '#74b9ff'
-    },
-    data: {
-        CHE: {
-            fillKey: 'VISITED',
-            cities: "Geneve, Lausanne, Zurich"
-        },
-        IDN: {
-            fillKey: 'VISITED',
-            cities: "Denpasar, Depok, Jakarta"
-        },
-    },
-    geographyConfig: {
-        popupTemplate: function(geo, data) {
-            return ['<div class="hoverinfo"><strong>',
-                    'Places I visited ' + geo.properties.name,
-                    ': ' + data.cities,
-                    '</strong></div>'].join('');
-        }
-    }
-});
+    var map;
 
-// resize with jQuery
-$(window).on('resize', function() {
-    map.resize();
-});
+    d3.csv("/data/travel.csv", function(error, data){
+        var totalCountry = 0;
+        var totalCities = 0;
+        var mapdata = {};
+        data.forEach(function(d){
+            totalCountry += 1
+            totalCities += d.cities.split(",").length
+            mapdata[d.country] = {
+                fillKey: 'VISITED',
+                cities: d.cities,
+            }
+        })
+        $("#countries").text(totalCountry)
+        $("#cities").text(totalCities)
 
+        map = new Datamap({
+            element: document.getElementById('container'),
+            responsive: true,
+            fills: {
+                VISITED: '#fab1a0',
+                defaultFill: '#74b9ff'
+            },
+            data: mapdata,
+            geographyConfig: {
+                popupTemplate: function(geo, data) {
+                    return ['<div class="hoverinfo"><strong>',
+                            'Places I visited in ' + geo.properties.name,
+                            ': ' + data.cities,
+                            '</strong></div>'].join('');
+                }
+            }
+        });
+    })
+
+    // resize with jQuery
+    $(window).on('resize', function() {
+        map.resize();
+    });
+})
