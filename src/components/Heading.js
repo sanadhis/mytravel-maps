a28780a736824e11a5f5cefb7802c9cb5e64a5b9
sanadhis <sanadhis@gmail.com>
2019-04-02 13:54:02 +0200

    Minor

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -27,16 +27,15 @@ class Heading extends Component {
       <div class="container">
         <div class="row">
           <div class="col-2 align-self-center">
-            <a href="javascript:history.back()"
-              class="btn btn-success float-right btn-back">
+            <a href="javascript:history.back()" class="btn btn-success float-right btn-back">
               Back <i class="fa fa-arrow-left" aria-hidden="true"></i>
             </a>
           </div>
           <div class="col-10">
             <p class="text-left summary">
               I have been into <span id="countries" class="info">{totalCountries}</span> countries
-                    and <span id="cities" class="info">{totalCities}</span> cities
-                </p>
+              and <span id="cities" class="info">{totalCities}</span> cities
+            </p>
             <p class="summary subsummary">
               As of <span id="now" class="info">{consts.time}</span>
             </p>
